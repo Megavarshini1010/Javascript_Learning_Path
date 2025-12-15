de69bc3960ce43f782fafdc7c0a177e40f41a9dd
Megavarshini R <144305447+Megavarshini1010@users.noreply.github.com>
2025-12-15 19:39:23 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,33 @@
+/*
+
+Topic = js variables
+This file covers:
+- var,let and const
+-naming rules
+-scope
+-de-clare & de-assign
+*/
+//*********let,var and const ********* */
+//var keyword declaration
+var age ; //declare
+ age = 20; // initilize //assign
+ var age =40; //re -declare
+age = 56; //re-initialize  // re-assign
+console.log(age);
+let na;
+na="mega";
+// let na="harshu"  --->   No re-declare
+ na="harshu";
+console.log(na);
+
+//*******naming rules**********
+// not start with number 
+//cannot use keywords
+//can start with symbols
+
+//**********scope*********
+//let and const = to respect block scope
+//var = function scope
+
+
+
